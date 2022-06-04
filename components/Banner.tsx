ebb2b3a50eb2c0bb4eb94da4cfabf3df272a4f0a
morshedulmunna <morshedulmunna1@gmail.com>
2022-06-04 20:25:04 +0600

    banner Design Done

---
+++ components/Banner.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { Movie } from "../tepings";
+import { FaPlay } from "react-icons/fa";
+import { InformationCircleIcon } from "@heroicons/react/outline";
+import { baseUrl } from "../constants/movie";
+import Image from "next/image";
+// import { useRecoilState } from "recoil";
+
+interface Props {
+  netflixOriginals: Movie[];
+}
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  return (
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[70vh] lg:justify-end lg:pb-12 px-6 md:px-12 lg:px-16">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          layout="fill"
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          objectFit="cover"
+        />
+      </div>
+
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+      <div className="flex space-x-3">
+        <button className="flex items-center w-[100px] justify-center rounded-lg text-black bg-white bannerButton">
+          <FaPlay className="w-4 h-4 pr-2 text-black md:h-7 md:w-7" />
+          Play
+        </button>
+
+        <button
+          className="bannerButton bg-[gray]/70"
+          //   onClick={() => {
+          //     setCurrentMovie(movie)
+          //     setShowModal(true)
+          //   }}
+        >
+          <InformationCircleIcon className="w-5 h-5 md:h-8 md:w-8 lg:w-28" />{" "}
+          More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,41 @@
-import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import { Movie } from "../tepings";
+import requests from "../utils/requests";
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
+
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   return (
-    <div>
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511]">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main>
-        {/* Banner */}
+        <Banner netflixOriginals={netflixOriginals} />
         <section>
-          {/* row*/}
-          {/* row*/}
-          {/* row*/}
-          {/* row*/}
-          {/* row*/}
-          {/* row*/}
           {/* row*/}
           {/* row*/}
           {/* row*/}
@@ -32,3 +48,51 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+/**
+ * All Data Fatching
+ * @returns {JSX.Element}
+ */
+
+export const getServerSideProps = async () => {
+  // const products = await getProducts(payments, {
+  //   includePrices: true,
+  //   activeOnly: true,
+  // })
+  //   .then((res) => res)
+  //   .catch((error) => console.log(error.message));
+
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+      //   products,
+    },
+  };
+};
